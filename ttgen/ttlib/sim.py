f656ed0cbc5d5f21b052f83c828cd48ca000cd54
quittung <quittung@users.noreply.github.com>
2021-10-19 21:22:28 +0200

    improved code analysis speed by splitting file
    vs code + pylance stopped working on my pc for sim.py

--- ttgen/ttlib/sim.py
+++ ttgen/ttlib/sim.py
@@ -2,17 +2,13 @@ from . import time3600 as t36
 from . import state as m_state
 from . import schedule as m_sched
 from . import sigsearch
+from .simhelper import *
 
 
 
 verbose = False
 time_step = 15 # HACK
 
-def time_add(time, duration, delta):
-    time = t36.timeShift(time, delta)
-    duration += delta
-
-    return time, duration
 
 def simulate_state(state: m_state.State):
     blockTable = generateBlocktable(time_step)
@@ -102,34 +98,6 @@ def simulate_state(state: m_state.State):
     score = conflict_schedule.wait_station_nobuffer * 0.5 + conflict_schedule.block_travel + conflict_schedule.block_station * 3
     return score
 
-class Conflict:
-    def __init__(self, ref = None) -> None:
-        self.wait_station_buffer: float = 0
-        self.wait_station_nobuffer: float = 0
-        self.block_station: float = 0
-        self.block_travel: float = 0
-        
-        if not ref == None:
-            self.wait_station_buffer = ref.wait_station_buffer
-            self.wait_station_nobuffer = ref.wait_station_nobuffer
-            self.block_station = ref.block_station
-            self.block_travel = ref.block_travel
-    
-    def __add__(self, other):
-        result = Conflict()
-
-        result.wait_station_buffer = self.wait_station_buffer + other.wait_station_buffer
-        result.wait_station_nobuffer = self.wait_station_nobuffer + other.wait_station_nobuffer
-        result.block_station = self.block_station + other.block_station 
-        result.block_travel = self.block_travel + other.block_travel 
-
-        return result
-
-    
-
-
-
-
 
 def wait_stop(wait_plan, wait_schedule, time, total_duration, block_table, path):
     waitTime = wait_plan + wait_schedule
@@ -176,28 +144,3 @@ def travel(sigdata, path, time, total_duration, block_table, block = True, wait 
             time, total_duration = time_add(time, total_duration, time_path)
 
     return time, total_duration, time_conflict
-
-
-def block_path(path, time, block_table) -> bool:
-    """reserves a path and returns if there were any conflicts"""
-    time_slot = t36.timeSlot(time, time_step)
-    conflict = is_blocked(path, time_slot, block_table)
-    block_table[time_slot].add(path)
-
-    return conflict
-
-def is_blocked(sigPath, time_slot, blockTable):
-    if sigPath in blockTable[time_slot]: return True
-    
-    depSig, arrSig =  sigPath.split("|")
-    if depSig + "|*" in blockTable[time_slot]: return True
-    if "*|" + arrSig in blockTable[time_slot]: return True
-
-    return False
-
-
-def generateBlocktable(timeStep):
-    bt = {}
-    for t in range(0, 3600, timeStep):
-        bt[t] = set()
-    return bt
