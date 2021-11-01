6f86beda5f9ab77e6f21f4a96808a554e472a523
quittung <quittung@users.noreply.github.com>
2021-11-01 19:06:19 +0100

    Update timediff.py

--- ttgen/timediff.py
+++ ttgen/timediff.py
@@ -3,11 +3,16 @@ def time_read(txt):
     second = int(txt[2:])
     return minute * 60 + second
 
+print("Calculates difference between to timestamps.")
+print("Format: mmss-mmss")
 while True:
-    txt = input()
+    txt = input("Enter expression: ")
     txt1, txt2 = txt.split("-")
     t1 = time_read(txt1)
     t2 = time_read(txt2)
 
-    print(t1 - t2)
+    delta = t1 - t2
+    if delta < 0: delta += 3600
+
+    print("{:.0f}\n".format(delta))
 
