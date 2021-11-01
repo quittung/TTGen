8594f1e7f69db311e03b9e49981c812a599b90c0
quittung <quittung@users.noreply.github.com>
2021-11-01 19:05:57 +0100

    added comments to ttgen.py

--- ttgen/ttgen.py
+++ ttgen/ttgen.py
@@ -1,4 +1,3 @@
-import os
 from ttlib import state as m_state, gmo_mini, timetable as tt, ttgraph
 
 
@@ -6,9 +5,11 @@ from ttlib import state as m_state, gmo_mini, timetable as tt, ttgraph
 timeStep = 15 # FIXME: Actually apply these
 verbose = False
 
+# calculate time table
 state = m_state.State()
 state = gmo_mini.gmo_search(state, visualize = False)
 
+# show time table
 timetable = tt.collect_timetable(state, True)
 ttgraph.graph(timetable)
 
